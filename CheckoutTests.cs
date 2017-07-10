bade5189c2564ad973f16a4ff6bdd038a580cb5d
Adnan <rafique.adnan@gmail.com>
2017-07-10 21:06:27 +0100

    REQ2, RED wrote failing test for item B returns 30

--- CheckoutTests.cs
+++ CheckoutTests.cs
@@ -19,5 +19,19 @@ namespace CheckoutKata
             //assert
             Assert.AreEqual(50,checkout.GetTotalPrice());
         }
+
+        [TestMethod]
+        public void GivenItemB_Return30()
+        {
+
+            //arrange
+
+            //act
+            var checkout = new Checkout();
+            checkout.Scan("B");
+
+            //assert
+            Assert.AreEqual(30, checkout.GetTotalPrice());
+        }
     }
 }
