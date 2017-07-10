e39a1fd583fe847c7658a0e887f05802458d1235
Adnan <rafique.adnan@gmail.com>
2017-07-10 22:22:34 +0100

    REQ5, REFACTOR, extracted discount from previous checkin to a Disounter class for better abstraction.  Idea is to pass the whole basket to the Discounter to process

--- Checkout.cs
+++ Checkout.cs
@@ -38,14 +38,32 @@ namespace CheckoutKata
 
             _discount = 0;
 
-            if (_basket.Count.Equals(3) && _item.Equals("A"))
-            {
-                _discount = -20;
-            }
+            _discount = new Discounter(_basket).CalculateDiscount(_item);
 
             return _total + _discount;
 
 
+        }
+
+
+    }
+
+    public class Discounter
+    {
+        private List<string> _basket;
+
+        public Discounter(List<string> basket)
+        {
+            _basket = basket;
+        }
+
+        public int CalculateDiscount(string item)
+        {
+            if (_basket.Count.Equals(3) && item.Equals("A"))
+            {
+                return -20;
+            }
+            return 0;
         }
     }
 }
