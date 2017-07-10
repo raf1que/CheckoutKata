9ed7211914ca9aa7d783740e9a9db8fa8dcb6b5f
Adnan <rafique.adnan@gmail.com>
2017-07-10 21:08:20 +0100

    REQ2 GREEN, added simple if statement to pass test

--- Checkout.cs
+++ Checkout.cs
@@ -2,13 +2,19 @@
 {
     public class Checkout :ICheckout
     {
+        private string _item;
+
         public void Scan(string item)
         {
-            
+            _item = item;
         }
 
         public int GetTotalPrice()
         {
+            if (_item.Equals("B"))
+            {
+                return 30;
+            }
             return 50;
         }
     }
