682413f2380c9ad0f7e8447820d05a46b76c8dd0
Adnan <rafique.adnan@gmail.com>
2017-07-10 22:24:28 +0100

    REQ5 REFACTOR(b) , oops forgot to transfer Discounter class into new file, easily done with resharper!

--- Checkout.cs
+++ Checkout.cs
@@ -41,29 +41,6 @@ namespace CheckoutKata
             _discount = new Discounter(_basket).CalculateDiscount(_item);
 
             return _total + _discount;
-
-
-        }
-
-
-    }
-
-    public class Discounter
-    {
-        private List<string> _basket;
-
-        public Discounter(List<string> basket)
-        {
-            _basket = basket;
-        }
-
-        public int CalculateDiscount(string item)
-        {
-            if (_basket.Count.Equals(3) && item.Equals("A"))
-            {
-                return -20;
-            }
-            return 0;
         }
     }
 }
---
+++ Discounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace CheckoutKata
+{
+    public class Discounter
+    {
+        private List<string> _basket;
+
+        public Discounter(List<string> basket)
+        {
+            _basket = basket;
+        }
+
+        public int CalculateDiscount(string item)
+        {
+            if (_basket.Count.Equals(3) && item.Equals("A"))
+            {
+                return -20;
+            }
+            return 0;
+        }
+    }
+}
